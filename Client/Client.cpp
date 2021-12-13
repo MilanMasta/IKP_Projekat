7ccd1e3954aec90de5c8a2b7837955f2c752259b
Milan Mastilovic <milanmastilovic1@gmail.com>
2021-12-13 01:24:55 +0100

    TCP konekcija

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,5 +1,6 @@
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
+// UDP client that uses blocking sockets
 #define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #define WIN32_LEAN_AND_MEAN
 
 #include <windows.h>
@@ -14,99 +15,129 @@
 #pragma comment (lib, "AdvApi32.lib")
 
 #define SERVER_IP_ADDRESS "127.0.0.1"
-#define SERVER_PORT 27016
-#define BUFFER_SIZE 256
+#define SERVER_PORT 15001
+#define BUFFER_SIZE 512
 
-// TCP client that use blocking sockets
+
 int main()
 {
-    // Socket used to communicate with server
-    SOCKET connectSocket = INVALID_SOCKET;
+	// Server address
+	sockaddr_in serverAddress;
 
-    // Variable used to store function return value
-    int iResult;
+	// Size of sockaddr_in structure
+	int sockAddrSize = sizeof(serverAddress);
 
-    // Buffer we will use to store message
-    char dataBuffer[BUFFER_SIZE];
+	// Buffer we will use to store message
+	char dataBuffer[BUFFER_SIZE];
 
-    // WSADATA data structure that is to receive details of the Windows Sockets implementation
-    WSADATA wsaData;
+	// Port on server that will be used for communication with client
+	unsigned short serverPort = SERVER_PORT;
 
-    // Initialize windows sockets library for this process
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-    {
-        printf("WSAStartup failed with error: %d\n", WSAGetLastError());
-        return 1;
-    }
+	// WSADATA data structure that is to receive details of the Windows Sockets implementation
+	WSADATA wsaData;
 
-    // create a socket
-    connectSocket = socket(AF_INET,
-        SOCK_STREAM,
-        IPPROTO_TCP);
+	// Initialize windows sockets for this process
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0)
+	{
+		printf("WSAStartup failed with error: %d\n", iResult);
+		return 1;
+	}
 
-    if (connectSocket == INVALID_SOCKET)
-    {
-        printf("socket failed with error: %ld\n", WSAGetLastError());
-        WSACleanup();
-        return 1;
-    }
+	// Initialize serverAddress structure
+	memset((char*)&serverAddress, 0, sockAddrSize);
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = inet_addr(SERVER_IP_ADDRESS);
+	serverAddress.sin_port = htons(serverPort);
 
-    // Create and initialize address structure
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;								// IPv4 protocol
-    serverAddress.sin_addr.s_addr = inet_addr(SERVER_IP_ADDRESS);	// ip address of server
-    serverAddress.sin_port = htons(SERVER_PORT);					// server port
+	// Create a socket
+	SOCKET clientSocket = socket(AF_INET,      // IPv4 address famly
+		SOCK_DGRAM,   // datagram socket
+		IPPROTO_UDP); // UDP
 
-    // Connect to server specified in serverAddress and socket connectSocket
-    if (connect(connectSocket, (SOCKADDR*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR)
-    {
-        printf("Unable to connect to server.\n");
-        closesocket(connectSocket);
-        WSACleanup();
-        return 1;
-    }
+// Check if socket creation succeeded
+	if (clientSocket == INVALID_SOCKET)
+	{
+		printf("Creating socket failed with error: %d\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
 
-    while (true) {
-        // Read string from user into outgoing buffer
-        printf("Enter message to send: ");
-        gets_s(dataBuffer, BUFFER_SIZE);
+	char previous_message[BUFFER_SIZE];
 
-        // Send message to server using connected socket
-        iResult = send(connectSocket, dataBuffer, (int)strlen(dataBuffer), 0);
+	while (true)
+	{
+		// Read string from user into outgoing buffer
+		printf("Enter message to send: ");
+		gets_s(dataBuffer, BUFFER_SIZE);
 
-        // Check result of send function
-        if (iResult == SOCKET_ERROR)
-        {
-            printf("send failed with error: %d\n", WSAGetLastError());
-            closesocket(connectSocket);
-            WSACleanup();
-            return 1;
-        }
+		// Check if client closing command is entered
+		if (!strcmp(dataBuffer, "stop client"))
+		{
+			break;
+		}
 
-        printf("Message successfully sent. Total bytes: %ld\n", iResult);
+		// Send message to server
+		iResult = sendto(clientSocket,
+			dataBuffer,
+			strlen(dataBuffer),
+			0,
+			(SOCKADDR*)&serverAddress,
+			sockAddrSize);
 
-        // Shutdown the connection since we're done
-        iResult = shutdown(connectSocket, SD_BOTH);
-
-        // Check if connection is succesfully shut down.
-        if (iResult == SOCKET_ERROR)
-        {
-            printf("Shutdown failed with error: %d\n", WSAGetLastError());
-            closesocket(connectSocket);
-            WSACleanup();
-            return 1;
-        }
-    }
-    // For demonstration purpose
-    printf("\nPress any key to exit: ");
-    _getch();
+		// Check result of send function
+		if (iResult == SOCKET_ERROR)
+		{
+			printf("sendto failed with error: %d\n", WSAGetLastError());
+			closesocket(clientSocket);
+			WSACleanup();
+			return 1;
+		}
 
 
-    // Close connected socket
-    closesocket(connectSocket);
+		// Check if 'server close command' is issued. Client is also closing, after server is closed.
+		//compare current and previous message	
+		if (!strcmp(previous_message, dataBuffer))
+		{
+			printf("Two identical messages were sent consecutively. Server is being closed.\n");
+			break;
+		}
+		//store current message for next comparison
+		strcpy(previous_message, dataBuffer);
 
-    // Deinitialize WSA library
-    WSACleanup();
 
-    return 0;
+
+		// Receive server message
+		iResult = recvfrom(clientSocket,
+			dataBuffer,
+			BUFFER_SIZE,
+			0,
+			(SOCKADDR*)&serverAddress,
+			&sockAddrSize);
+
+		if (iResult == SOCKET_ERROR)
+		{
+			printf("recvfrom failed with error: %d\n", WSAGetLastError());
+			continue;
+		}
+
+		// Set end of string
+		dataBuffer[iResult] = '\0';
+		// Log message from server
+		printf("Message from server: %s.\n\n", dataBuffer);
+
+	}
+
+	// Close application
+	iResult = closesocket(clientSocket);
+	if (iResult == SOCKET_ERROR)
+	{
+		printf("closesocket failed with error: %d\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+
+	WSACleanup();
+
+	return 0;
 }
