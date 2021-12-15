b1e6c53b004257281efe2a3a1f35cc83073b5b6c
Milan Mastilovic <milanmastilovic1@gmail.com>
2021-12-15 02:32:33 +0100

    MultiThred

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -8,7 +8,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "conio.h"
-
+#include "string.h"
+using namespace std;
 #pragma comment (lib, "Ws2_32.lib")
 #pragma comment (lib, "Mswsock.lib")
 #pragma comment (lib, "AdvApi32.lib")
@@ -17,86 +18,84 @@
 #define BUFFER_SIZE 256
 
 // TCP server that use blocking sockets
-
-DWORD WINAPI KonekcijaKorisnika(LPVOID lpParam)
-{
-    for (int i = 0;i < 1000;i++)
-    {
-        printf("1");
-    }
-    return 0;
-}
-
 #pragma region Strukture
-
 typedef struct poruka_st {
     char sadrzajPoruke[BUFFER_SIZE];
-    char posiljaoca[20];
+    char posiljaoc[20];
     struct poruka_st* next;
 } PORUKA;
 
 typedef struct klijent_st {
     char ime[20];
-    struct klijent_st* next;
+    unsigned int acptSocket;
     PORUKA* poruke;
+    struct klijent_st* next;
 } KLIJENT;
 
 #pragma endregion
 
-#pragma region UpravljanjeListom
-void init_list(KLIJENT** head){
-    *head = NULL;
+KLIJENT* kreiraj_novog_klijenta(char* ime, unsigned int socket);
+void add_to_list(KLIJENT* novi, KLIJENT** head);
+void citajKlijenta(KLIJENT** head);
+KLIJENT* pronadjiKlijenta(KLIJENT** head, char* imeKlijenta);
+KLIJENT* head = NULL;
+
+
+
+DWORD WINAPI inboxKorisnika(LPVOID lpParam) {
+
+ KLIJENT* klijent = (KLIJENT*)lpParam;
+ head;
+ unsigned int klijentSocket = klijent->acptSocket;
+ char* ime = klijent->ime;
+ char dataBuffer[BUFFER_SIZE];
+ int iResult = 1;
+ head;
+ while (true) {
+     while (iResult > 0)
+     {
+         // Receive data until the client shuts down the connection
+         iResult = recv(klijentSocket, dataBuffer, BUFFER_SIZE, 0);
+         if (iResult > 0)	// Check if message is successfully received
+         {
+             dataBuffer[iResult] = '\0';
+             char* primalac = strchr(dataBuffer, '\n') + 1;
+             char* poruka = strtok(dataBuffer, "\n");
+             // Log message text
+             printf("%s Je poslao poruku klijentu %s: %s\n", klijent->ime, primalac, poruka);
+
+         }
+         else if (iResult == 0)	// Check if shutdown command is received
+         {
+             // Connection was closed successfully
+             printf("Connection with client closed.\n");
+             closesocket(klijentSocket);
+         }
+         else	// There was an error during recv
+         {
+             printf("Klijent %s se diskonektovao\n", klijent->ime);
+             closesocket(klijentSocket);
+             iResult = -1;
+             iResult = shutdown(klijentSocket, SD_BOTH);
+             closesocket(klijentSocket);
+         }
+     } ;
+ }
+
+ return 0;
 }
 
-void add_to_list(KLIJENT* novi, KLIJENT** head) {
-    if (*head == NULL) { // list is empty
-        *head = novi;
-        return;
-    }
-
-    add_to_list(novi, &((*head)->next));
-}
-
-KLIJENT* kreiraj_novog_klijenta(char ime[]) {
-    KLIJENT* novi = (KLIJENT*)malloc(sizeof(KLIJENT));
-    if (novi == NULL) {
-        printf("Not enough RAM!\n");
-        exit(21);
-    }
-
-    strcpy(novi->ime, ime);
-    PORUKA* poruke = NULL;
-    novi->next = NULL;
-    printf("%s kreiran\n", ime);
-    return novi;
-}
-
-void dodaj_poruku_u_inbox(char ime[],char NoviSadrzajPoruka[],char posiljaoc[]) {
-    PORUKA* novaPoruka = (PORUKA*)malloc(sizeof(PORUKA));
-    if (novaPoruka == NULL) {
-        printf("Not enough RAM!\n");
-        exit(21);
-    }
-    strcpy(novaPoruka->sadrzajPoruke, NoviSadrzajPoruka);
-}
-#pragma endregion
-
-KLIJENT* head;
-int brojKorisnika = 0;
-
-int main()
+DWORD WINAPI KonekcijaKorisnika(LPVOID lpParam)
 {
-    init_list(&head);
-#pragma region Konekcija
+    KLIJENT** head = (KLIJENT**)lpParam;
     //inicijalizacija liste
-
-
+    int brojKorisnika = 0;
     // Socket used for listening for new clients 
     SOCKET listenSocket = INVALID_SOCKET;
 
     // Socket used for communication with client
     SOCKET acceptedSocket = INVALID_SOCKET;
-    
+
     // Variable used to store function return value
     int iResult;
 
@@ -110,7 +109,6 @@ int main()
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
     {
         printf("WSAStartup failed with error: %d\n", WSAGetLastError());
-        return 1;
     }
 
 
@@ -132,7 +130,6 @@ int main()
     {
         printf("socket failed with error: %ld\n", WSAGetLastError());
         WSACleanup();
-        return 1;
     }
 
     // Setup the TCP listening socket - bind port number and local address to socket
@@ -144,7 +141,6 @@ int main()
         printf("bind failed with error: %d\n", WSAGetLastError());
         closesocket(listenSocket);
         WSACleanup();
-        return 1;
     }
 
     // Set listenSocket in listening mode
@@ -154,9 +150,8 @@ int main()
         printf("listen failed with error: %d\n", WSAGetLastError());
         closesocket(listenSocket);
         WSACleanup();
-        return 1;
     }
-    
+
     printf("Server socket is set to listening mode. Waiting for new connection requests.\n");
 
     while (true) {
@@ -174,10 +169,7 @@ int main()
             printf("accept failed with error: %d\n", WSAGetLastError());
             closesocket(listenSocket);
             WSACleanup();
-            return 1;
         }
-
-        printf("\nNew client request accepted. Client address: %s : %d\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
 
         char ime[20];
         // Receive data until the client shuts down the connection
@@ -188,71 +180,115 @@ int main()
             ime[iResult] = '\0';
 
             if (brojKorisnika == 0) {
-                KLIJENT* novi = kreiraj_novog_klijenta(ime);
-                add_to_list(novi, &head);
+                KLIJENT* klijent = kreiraj_novog_klijenta(ime,acceptedSocket);
+                add_to_list(klijent, head);
                 brojKorisnika++;
+                DWORD KorisnikID;
+                HANDLE hinboxKorisnika;
+                hinboxKorisnika = CreateThread(NULL, 0, &inboxKorisnika, (LPVOID)klijent, 0, &KorisnikID);
             }
             else {
-                //provjera da li INBOX vec postoji treba da se uradi
-                KLIJENT* novi = kreiraj_novog_klijenta(ime);
-                add_to_list(novi, &head);
-                brojKorisnika++;
+                KLIJENT* ret = pronadjiKlijenta(head, ime);
+                if (ret == NULL) { // ne postoji taj klijent
+                    KLIJENT* klijent = kreiraj_novog_klijenta(ime, acceptedSocket);
+                    add_to_list(klijent, head);
+                    brojKorisnika++;
+                    DWORD KorisnikID;
+                    HANDLE hinboxKorisnika;
+                    hinboxKorisnika = CreateThread(NULL, 0, &inboxKorisnika, (LPVOID)klijent, 0, &KorisnikID);
+                    citajKlijenta(head);
+                }
+                else {
+                    KLIJENT* klijent = (*head);
+                    while (klijent != NULL)
+                    {
+                        if (!strcmp(klijent->ime, ime)) {
+                            klijent->acptSocket = acceptedSocket;
+                            DWORD KorisnikID;
+                            HANDLE hinboxKorisnika;
+                            hinboxKorisnika = CreateThread(NULL, 0, &inboxKorisnika, (LPVOID)klijent, 0, &KorisnikID);
+                            printf("%s se opet konektovao\n", ime);
+                        }
+                        klijent = klijent->next;
+                    }
+                    citajKlijenta(head);
+                }
             }
         }
     }
-#pragma endregion
-
-            
-#pragma region NeblokirajucePrimanje
-            unsigned long mode = 1; //non-blocking mode
-            int iResult2 = ioctlsocket(acceptedSocket, FIONBIO, &mode);
-            char dataBuffer2[BUFFER_SIZE];
-            if (iResult2 != NO_ERROR)
-                printf("ioctlsocket failed with error: %ld\n", iResult2);
-            while (true) {
-
-                // Poziv operacije nad soketom
-                iResult2 = recv(acceptedSocket, dataBuffer2, BUFFER_SIZE, 0);
-                //Provera da li se operacije uspesno izvrsila
-                if (iResult2 != SOCKET_ERROR) {
-                    dataBuffer2[iResult2] = '\0';
-                    printf("Client sent neblokirajuci: %s.\n", dataBuffer2);
-                }
-                else
-                {
-                    if (WSAGetLastError() == WSAEWOULDBLOCK) {
-                        // U pitanju je blokirajuca operacija koja zbog rezima
-                        // soketa nece biti izvrsena
-                    }
-                    else {
-                        // Desila se neka druga greska prilikom poziva operacije
-                        printf("Greska");
-                    }
-                }
-            }
-#pragma endregion
-
-#pragma region Zatvaranje
-
-
-            // Shutdown the connection since we're done
-    iResult = shutdown(acceptedSocket, SD_BOTH);
-
-    // Check if connection is succesfully shut down.
-    if (iResult == SOCKET_ERROR)
-    {
-        printf("shutdown failed with error: %d\n", WSAGetLastError());
-        closesocket(acceptedSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    //Close listen and accepted sockets
-    closesocket(listenSocket);
-    closesocket(acceptedSocket);
-
-    // Deinitialize WSA library
-    WSACleanup();
-#pragma endregion
     return 0;
 }
+
+
+#pragma region UpravljanjeListom
+
+
+
+KLIJENT* pronadjiKlijenta(KLIJENT** head, char* imeKlijenta) {
+    KLIJENT* ret = NULL;
+    KLIJENT** temp = head;
+    while ((*temp) != NULL)
+    {
+        if (!strcmp((*temp)->ime, imeKlijenta)) {
+            return (*temp);
+        }
+        (*temp) = (*temp)->next;
+    }
+    head;
+    return ret;
+}
+void citajKlijenta(KLIJENT** head) {
+    KLIJENT** temp = head;
+    while (*temp != NULL)
+    {
+        printf("%s %u \n", (*temp)->ime, (*temp)->acptSocket);
+        (*temp) = (*temp)->next;
+    }
+}
+
+void add_to_list(KLIJENT* novi, KLIJENT** head) {
+    if (*head == NULL) { // list is empty
+        *head = novi;
+        return;
+    }
+    add_to_list(novi, &((*head)->next));
+}
+
+KLIJENT* kreiraj_novog_klijenta(char* ime, unsigned int socket) {
+    KLIJENT* novi = (KLIJENT*)malloc(sizeof(KLIJENT));
+
+    if (novi == NULL) {
+        printf("Not enough RAM!\n");
+        exit(21);
+    }
+
+    strcpy(novi->ime, ime);
+    novi->acptSocket = socket;
+    novi->next = NULL;
+    novi->poruke = NULL;
+    printf("%s se konektovao\n", ime);
+    return novi;
+}
+
+void dodaj_poruku_u_inbox(char* NoviSadrzajPoruka,char* posiljaoc,char* primalac) {
+    PORUKA* novaPoruka = (PORUKA*)malloc(sizeof(PORUKA));
+    if (novaPoruka == NULL) {
+        printf("Not enough RAM!\n");
+        exit(21);
+    }
+    strcpy(novaPoruka->sadrzajPoruke, NoviSadrzajPoruka);
+    strcpy(novaPoruka->posiljaoc, posiljaoc);
+}
+#pragma endregion
+
+int main()
+{
+    DWORD KonekcijaKorisnikaID;
+    HANDLE hKonekcijaKorisnika;
+    hKonekcijaKorisnika = CreateThread(NULL, 0, &KonekcijaKorisnika, (LPVOID)&head, 0, &KonekcijaKorisnikaID);
+    
+
+    _getch();
+    return 0;
+}
+
